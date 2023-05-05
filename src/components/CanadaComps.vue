70a95668b3fbf3cbefee1c98498ab5dc3b95e3f3
Esti Tweg <estictweg@hotmail.com>
2023-05-05 23:19:02 +0930

    add more comp results. reformat

--- src/components/CanadaComps.vue
+++ src/components/CanadaComps.vue
@@ -1,78 +1,120 @@
 <template>
-    <div id="">
-        <h4>2020</h4>
-        <a class="link nolink" target="_blank" href="">?? Tour de Bloc @ Le Mouv' espace bloc, Montreal - 08/02/2020 </a> 
-        <h4>2019</h4>
-        <a class="link nolink" target="_blank" href="">?? @ Central Rock Gym, NYC - 31/10/2019 </a> 
-        <a class="link nolink" target="_blank" href="">2nd @ Brooklyn Boulders, NYC - 07/09/2019 </a> 
-        <a class="link nolink" target="_blank" href="">?? @ Central Rock Gym, NYC - 25/08/2019 </a> 
-        <a class="link nolink" target="_blank" href="">2nd UBS @ Aspire Climbing, Milton - 02/03/2019 </a> 
-        <h4>2018</h4>
-        <a class="link nolink" target="_blank" href="">?? UBS @ Toprock Climbing, Brampton - 24/11/2018 </a> 
-        <a class="link nolink" target="_blank" href="">3rd @ Beyond Bouldering, Adelaide - 27/05/2018 </a> 
-        <a class="link nolink" target="_blank" href="">1st @ ABC, Adelaide - ??/2018 </a> 
-        <a class="link nolink" target="_blank" href="">3rd @ Beyond Bouldering, Adelaide - 27/05/2018 </a> 
-        <a class="link nolink" target="_blank" href="">2nd Lead State Titles @ VRC, Adelaide - 03/03/2018 </a> 
-        <a class="link nolink" target="_blank" href="">Boulder National Series OCF  @ Boulderz, Etobicoke - 27/01/2018 </a> 
-        <h4>2017</h4>
-        <a class="link nolink" target="_blank" href="">4th Tour de Bloc @ Coyote Rock Gym, Ottawa  - 04/02/2016 </a> 
-        <h4>2016</h4>
-        <a class="link nolink" target="_blank" href="">?? University Boulder Series @ Boulderz, Etobicoke - 26/03/2016 </a> 
-        <a class="link nolink" target="_blank" href="">14th Tour de Bloc Nationals @ Bloc Shop, Montreal - 12/03/2016 </a> 
-        <a class="link nolink" target="_blank" href="">3rd Tour de Bloc @ Grand River Rocks, Kitchener - 20/02/2016 </a> 
-        <a class="link nolink" target="_blank" href="">4th Tour de Bloc @ Coyote, Ottawa - 06/02/2016 </a> 
-        <a class="link nolink" target="_blank" href="">11th CEC Boulder National Series Event @ True North Climbing, Toronto  - 30/01/2016 </a> 
-        <h4>2015</h4>
-        <a class="link nolink" target="_blank" href="">1st National Capital Boulder Series @ Coyote Rock Gym, Ottawa - 14/12/2015 </a>  
+  <div id="">
+    <h4>2020</h4>
+    <a class="link nolink" target="_blank" href="">?? Tour de Bloc @ Le Mouv' espace bloc, Montreal - 08/02/2020 </a>
+    <h4>2019</h4>
+    <a class="link nolink" target="_blank" href="">?? @ Central Rock Gym, NYC - 31/10/2019 </a>
+    <a class="link nolink" target="_blank" href="">2nd @ Brooklyn Boulders, NYC - 07/09/2019 </a>
+    <a class="link nolink" target="_blank" href="">?? @ Central Rock Gym, NYC - 25/08/2019 </a>
+    <a class="link nolink" target="_blank" href="">2nd UBS @ Aspire Climbing, Milton - 02/03/2019 </a>
+    <h4>2018</h4>
+    <a class="link nolink" target="_blank" href="">?? UBS @ Toprock Climbing, Brampton - 24/11/2018 </a>
+    <a class="link nolink" target="_blank" href="">3rd @ Beyond Bouldering, Adelaide - 27/05/2018 </a>
+    <a class="link nolink" target="_blank" href="">1st @ ABC, Adelaide - ??/2018 </a>
+    <a class="link nolink" target="_blank" href="">3rd @ Beyond Bouldering, Adelaide - 27/05/2018 </a>
+    <a class="link nolink" target="_blank" href="">2nd Lead State Titles @ VRC, Adelaide - 03/03/2018 </a>
+    <a class="link nolink" target="_blank" href="">Boulder National Series OCF @ Boulderz, Etobicoke - 27/01/2018 </a>
+    <h4>2017</h4>
+    <a class="link nolink" target="_blank" href="">4th Tour de Bloc @ Coyote Rock Gym, Ottawa - 04/02/2016 </a>
+    <h4>2016</h4>
+    <a class="link nolink" target="_blank" href="">?? University Boulder Series @ Boulderz, Etobicoke - 26/03/2016 </a>
+    <a class="link nolink" target="_blank" href="">14th Tour de Bloc Nationals @ Bloc Shop, Montreal - 12/03/2016 </a>
+    <a class="link nolink" target="_blank" href="">3rd Tour de Bloc @ Grand River Rocks, Kitchener - 20/02/2016 </a>
+    <a class="link nolink" target="_blank" href="">4th Tour de Bloc @ Coyote, Ottawa - 06/02/2016 </a>
+    <a class="link nolink" target="_blank" href="">11th CEC Boulder National Series Event @ True North Climbing, Toronto -
+      30/01/2016 </a>
+    <h4>2015</h4>
+    <a class="link nolink" target="_blank" href="">1st National Capital Boulder Series @ Coyote Rock Gym, Ottawa -
+      14/12/2015 </a>
 
-        <a class="link nolink" target="_blank" href="">3rd FQME Boulder National Series Event @ Délire Escalade, Quebec City - 29/11/2015 </a>  
-        <a class="link nolink" target="_blank" href="">5th Tour de Bloc @ Altitude, Gatineau - 21/11/2015 </a>  
-        <a class="link nolink" target="_blank" href="">4th National Capital Boulder Series @ Vertical Reality, Ottawa - 24/10/2015 </a>  
-        <a class="link nolink" target="_blank" href="">8th Tour de Bloc @ Allez Up, Montreal - 24/10/2015 </a>  
-        <a class="link nolink" target="_blank" href="">?? Summer Sweat Fest Finale @ Climbers Rock, Burlington - 19/09/2015 </a>  
-        <a class="link nolink" target="_blank" href="">?? Summer Sweat Fest @ The Hub, Markham - 11/08/2015 </a>  
-        <a class="link nolink" target="_blank" href="">2nd Summer Sweat Fest @ Rock Oasis, Toronto - 23/07/2015 </a>  
-        <a class="link nolink" target="_blank" href="">9th Summer Sweat Fest @ True North Climbing, Toronto - 09/07/2015 </a>  
-        <a class="link nolink" target="_blank" href="">10th Psico Bloc @ Olympic Stadium, Quebec City - 04/07/2015 </a>  
-        <a class="link nolink" target="_blank" href="">?? Summer Sweat Fest @ Boulderz, Etobicoke - 11/06/2015 </a>  
-        <a class="link nolink" target="_blank" href="">5th Difficulty Nationals @ The Boulders, Victoria - 16/05/2015 </a>  
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=34">5th OCF Difficulty Local @ Boulderz, Etobicoke - 11/04/2015</a>  
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=33">1st OCF Difficulty Local @ Junction Climbing Center, London - 28/03/2015</a>  
-        <a class="link nolink" target="_blank" href="">27th CEC Open Boulder Nationals @ Bloc Shop, Montreal - 21/03/2015</a>
-        <a class="link nolink" target="_blank" href="">3rd Open Boulder National Series Event @ Joe Rock Heads, Toronto - 07/03/2015</a>    
-        <a class="link nolink" target="_blank" href="">2nd Tour de Bloc @ Grand River Rocks, Kitchener - 21/02/2015</a>  
-        <a class="link" :class="{hide: showAll}" target="_blank" href="https://squamishclimbingmagazine.ca/final-results-2015-youth-bouldering-nationals/">2nd Youth Boulder Nationals @ Climbers Rock, Burlington - 13/02/2015</a>
-        <a class="link nolink" target="_blank" href="">2nd Tour de Bloc @ Vertical, Montreal - 24/01/2015</a>  
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=29">1st OCF Bouldering Provincials @ Boulderz, Etobicoke - 18/01/2015</a>  
-        <a class="link " target="_blank" href="https://gripped.com/news/tour-de-bloc-climbers-rock-brock-briggs-triumphs-bloc-buster/">3rd Tour De Bloc @ Climber's Rock, Burlington - 10/01/2015</a>  
-        <h4>2014</h4>
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=28">2nd OCF Bouldering Local @ True North Climbing, Toronto - 13/12/2014</a>  
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=27">2nd OCF Bouldering Local @ Climber's Rock, Burlington - 22/11/2014</a>  
-        <a class="link nolink" target="_blank" href="">3rd Tour De Bloc @ True North Climbing - 08/11/2014</a>  
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=26">2nd OCF Bouldering Local @ Junction Climbing Center, London - 02/11/2014</a>  
-        <a class="link nolink" target="_blank" href="">?? Tour De Bloc @ Allez Up - 25/10/2014</a>  
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=25">2nd OCF Bouldering Local @ Rock Oasis, Toronto - 11/10/2014</a>  
-        <a class="link nolink" target="_blank" href="">?? Summer Sweat Fest Finale @ The Hub, Markham - 20/09/2014</a>  
-        <a class="link nolink" target="_blank" href="">?? Summer Sweat Fest @ Boulderz, Etobicoke - 21/08/2014</a>  
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=24">7th Eastern Difficulty Regionals @ True North Climbing - 19/04/2014</a>  
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=22">1st OCF Difficulty Provincials @ Grand River Rocks, Kitchener - 05/04/2014</a>  
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=21">2nd OCF Difficulty Local @ Guelph Grotto, Guelph - 22/03/2014</a>    
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=20">2nd OCF Difficulty Local @ Rock Oasis, Toronto - 02/03/2014</a>    
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=19">2nd OCF Difficulty Local @ Of Rock and Chalk, Newmarket - 16/02/2014</a>    
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=17">3rd OCF Bouldering Provincials @ Climber's Rock, Burlington - 02/02/2014</a>    
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=16">3rd OCF Bouldering Local @ True North Climbing, Toronto - 18/01/2014</a>   
-        <h4>2013</h4> 
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=15">2nd OCF Bouldering Local @ Rock Oasis, Toronto - 24/11/2013</a>    
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=14">2nd OCF Bouldering Local @ Coyote Rock Gym, Ottawa - 26/10/2013</a>    
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=10">3rd OCF Difficulty Provincials @ Climbers Rock, Burlington - 14/04/2013</a>    
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=9">7nd OCF Difficulty Local @ Guelph Grotto, Guelph - 30/03/2013</a>    
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=8">2nd OCF Difficulty Local @ Rock Oasis, Toronto - 02/03/2013</a>    
-        <a class="link" target="_blank" href="https://my.climbontario.ca/docs/OCFBoulderingprovincial2013RESULTS.pdf">3rd OCF Bouldering Provincials @ Boulderz, Toronto - 23/02/2013</a>    
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=5">3rd OCF Difficulty Local @ Grand River Rocks, Kitchener - 26/01/2013</a>    
-        <h4>2012</h4>
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=3">2nd OCF Difficulty Local @ Coyote Rock Gym, Ottawa - 08/12/2012</a>
-        <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=1&cat=3">2nd OCF Difficulty Local @ Peaks, St. Catherines - 14/10/2012</a>
-    </div>
+    <a class="link nolink" target="_blank" href="">3rd FQME Boulder National Series Event @ Délire Escalade, Quebec City -
+      29/11/2015 </a>
+    <a class="link nolink" target="_blank" href="">5th Tour de Bloc @ Altitude, Gatineau - 21/11/2015 </a>
+    <a class="link nolink" target="_blank" href="">4th National Capital Boulder Series @ Vertical Reality, Ottawa -
+      24/10/2015 </a>
+    <a class="link nolink" target="_blank" href="">8th Tour de Bloc @ Allez Up, Montreal - 24/10/2015 </a>
+    <a class="link nolink" target="_blank" href="">?? Summer Sweat Fest Finale @ Climbers Rock, Burlington - 19/09/2015
+    </a>
+    <a class="link nolink" target="_blank" href="">?? Summer Sweat Fest @ The Hub, Markham - 11/08/2015 </a>
+    <a class="link nolink" target="_blank" href="">2nd Summer Sweat Fest @ Rock Oasis, Toronto - 23/07/2015 </a>
+    <a class="link nolink" target="_blank" href="">9th Summer Sweat Fest @ True North Climbing, Toronto - 09/07/2015 </a>
+    <a class="link nolink" target="_blank" href="">10th Psico Bloc @ Olympic Stadium, Quebec City - 04/07/2015 </a>
+    <a class="link nolink" target="_blank" href="">?? Summer Sweat Fest @ Boulderz, Etobicoke - 11/06/2015 </a>
+    <a class="link nolink" target="_blank" href="">5th Difficulty Nationals @ The Boulders, Victoria - 16/05/2015 </a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=34">5th OCF Difficulty Local @ Boulderz,
+      Etobicoke - 11/04/2015</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=33">1st OCF Difficulty Local @ Junction
+      Climbing Center, London - 28/03/2015</a>
+    <a class="link nolink" target="_blank" href="">27th CEC Open Boulder Nationals @ Bloc Shop, Montreal - 21/03/2015</a>
+    <a class="link nolink" target="_blank" href="">3rd Open Boulder National Series Event @ Joe Rock Heads, Toronto -
+      07/03/2015</a>
+    <a class="link nolink" target="_blank" href="">2nd Tour de Bloc @ Grand River Rocks, Kitchener - 21/02/2015</a>
+    <a class="link" :class="{ hide: showAll }" target="_blank"
+      href="https://squamishclimbingmagazine.ca/final-results-2015-youth-bouldering-nationals/">2nd Youth Boulder
+      Nationals @ Climbers Rock, Burlington - 13/02/2015</a>
+    <a class="link nolink" target="_blank" href="">2nd Tour de Bloc @ Vertical, Montreal - 24/01/2015</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=29">1st OCF Bouldering Provincials @
+      Boulderz, Etobicoke - 18/01/2015</a>
+    <a class="link " target="_blank"
+      href="https://gripped.com/news/tour-de-bloc-climbers-rock-brock-briggs-triumphs-bloc-buster/">3rd Tour De Bloc @
+      Climber's Rock, Burlington - 10/01/2015</a>
+    <h4>2014</h4>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=28">2nd OCF Bouldering Local @ True
+      North Climbing, Toronto - 13/12/2014</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=27">2nd OCF Bouldering Local @ Climber's
+      Rock, Burlington - 22/11/2014</a>
+    <a class="link nolink" target="_blank" href="">3rd Tour De Bloc @ True North Climbing - 08/11/2014</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=26">2nd OCF Bouldering Local @ Junction
+      Climbing Center, London - 02/11/2014</a>
+    <a class="link nolink" target="_blank" href="">?? Tour De Bloc @ Allez Up - 25/10/2014</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=25">2nd OCF Bouldering Local @ Rock
+      Oasis, Toronto - 11/10/2014</a>
+    <a class="link nolink" target="_blank" href="">?? Summer Sweat Fest Finale @ The Hub, Markham - 20/09/2014</a>
+    <a class="link nolink" target="_blank" href="">?? Summer Sweat Fest @ Boulderz, Etobicoke - 21/08/2014</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=24">7th Eastern Difficulty Regionals @
+      True North Climbing - 19/04/2014</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=22">1st OCF Difficulty Provincials @
+      Grand River Rocks, Kitchener - 05/04/2014</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=21">2nd OCF Difficulty Local @ Guelph
+      Grotto, Guelph - 22/03/2014</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=20">2nd OCF Difficulty Local @ Rock
+      Oasis, Toronto - 02/03/2014</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=19">2nd OCF Difficulty Local @ Of Rock
+      and Chalk, Newmarket - 16/02/2014</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=17">3rd OCF Bouldering Provincials @
+      Climber's Rock, Burlington - 02/02/2014</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=16">3rd OCF Bouldering Local @ True
+      North Climbing, Toronto - 18/01/2014</a>
+    <h4>2013</h4>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=15">2nd OCF Bouldering Local @ Rock
+      Oasis, Toronto - 24/11/2013</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=14">2nd OCF Bouldering Local @ Coyote
+      Rock Gym, Ottawa - 26/10/2013</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=10">3rd OCF Difficulty Provincials @
+      Climbers Rock, Burlington - 14/04/2013</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=9">7nd OCF Difficulty Local @ Guelph
+      Grotto, Guelph - 30/03/2013</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=8">2nd OCF Difficulty Local @ Rock
+      Oasis, Toronto - 02/03/2013</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/docs/OCFBoulderingprovincial2013RESULTS.pdf">3rd OCF
+      Bouldering Provincials @ Boulderz, Toronto - 23/02/2013</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=5">3rd OCF Difficulty Local @ Grand
+      River Rocks, Kitchener - 26/01/2013</a>
+    <h4>2012</h4>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=3">2nd OCF Difficulty Local @ Coyote
+      Rock Gym, Ottawa - 08/12/2012</a>
+    <a class="link" target="_blank" href="https://my.climbontario.ca/dates.php?id=1&cat=3">2nd OCF Difficulty Local @
+      Peaks, St. Catherines - 14/10/2012</a>
+    <h4>2011</h4>
+    <a class="link nolink" target="_blank" href="">?? Tour De Bloc @ Joe Rock Heads, Toronto - ??/11/2011</a>
+    <a class="link nolink" target="_blank" href="">2nd @ Climber's Rock, Burlington - 16/04/2011</a>
+    <a class="link nolink" target="_blank" href="">3rd Youth Provincials @ True North Climbing, Toronto - 04/04/2011</a>
+    <a class="link nolink" target="_blank" href="">3rd Tour de Bloc @ True North Climbing, Toronto - 27/02/2011</a>
+    <a class="link nolink" target="_blank" href="">3rd Tour de Bloc @ Gravity, Hamilton - 12/02/2011</a>
+    <a class="link nolink" target="_blank" href="">3rd Youth @ Boulderz Junction, Toronto - 10/01/2011</a>
+    <h4>2010</h4>
+    <a class="link nolink" target="_blank" href="">3rd Tour de Bloc @ Climber's Rock, Burlington - 29/03/2010</a>
+  </div>
 </template>
 
 <script setup>
@@ -88,10 +130,11 @@ onMounted(() => {
 @import '../scss/mixins.scss';
 
 h4 {
-    margin-top: 1rem;
+  margin-top: 1rem;
 }
 
-a, p {
+a,
+p {
   font-weight: 500;
   font-size: 1rem;
   display: block;
@@ -101,6 +144,7 @@ span {
   font-size: 0.8rem;
   font-weight: 500;
 }
+
 .link {
   cursor: pointer;
   text-decoration: none;
@@ -116,7 +160,7 @@ span {
 }
 
 .nolink:hover {
-    text-decoration: none;
-    cursor: inherit;
+  text-decoration: none;
+  cursor: inherit;
 }
 </style>
